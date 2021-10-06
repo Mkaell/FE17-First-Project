62fed85ff6cf3e026fbfd0257b91db061c144bbc
Mkaell <mrohacevich@yandex.ru>
2021-10-06 21:28:26 +0300

    Changed size in the section CONTACT

---
+++ js/script.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger'),
+        back = document.querySelector('.back');
+
+
+    menuItem.forEach(item => {
+        toggleMenu(item);
+    });
+
+
+    function toggleMenu(element){
+        element.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+            back.classList.toggle('show');
+        });
+    }
+
+    toggleMenu(hamburger);
+    toggleMenu(back);
+
+});
