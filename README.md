00301153a052d2c6ed5e7c6d517e41be6d614ec8
Mkaell <mrohacevich@yandex.ru>
2022-01-06 15:24:16 +0300

    Readme.md changed

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# FE17-First-Project 
-https://mkaell.github.io/FE17-First-Project/
+# [AXIT](https://mkaell.github.io/FE17-First-Project/)
+
+### _Technologies used:_ 
+- The application is written using **JS**.
+-  There is an adaptation for all devices.
+
+
+<div align="center">
+  <img src="./img/screens/first.png" align="center" width='49%'>
+  <img src="./img/screens/second.png"align="center" width='49%'>
+  <img src="./img/screens/third.png"align="center" width='49%'>
+  <img src="./img/screens/fourth.png"align="center" width='49%'>
+</div>
--- js/script.js
+++ js/script.js
@@ -1,60 +1,59 @@
-window.addEventListener('DOMContentLoaded', () => {
-    //hamburger
-    const menu = document.querySelector('.header__list'),
+//hamburger
+const menu = document.querySelector('.header__list'),
         menuItem = document.querySelectorAll('.header__item'),
         hamburger = document.querySelector('.hamburger');
-        // back = document.querySelector('.back');
+    // back = document.querySelector('.back');
 
 
-    menuItem.forEach(item => {
-        toggleMenu(item);
+menuItem.forEach(item => {
+    toggleMenu(item);
+});
+
+
+function checkDisplay() {
+    if(window.getComputedStyle(hamburger, null).getPropertyValue("display") == 'block') {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__list-active');
+        document.body.classList.toggle('overflow');
+        // back.classList.toggle('show');
+    }
+}
+
+function toggleMenu(element){
+    element.addEventListener('click', () => {
+        checkDisplay();
     });
 
+}
+toggleMenu(hamburger);
 
-    function checkDisplay() {
-        if(window.getComputedStyle(hamburger, null).getPropertyValue("display") == 'block') {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('header__list-active');
-            document.body.classList.toggle('overflow');
-            // back.classList.toggle('show');
+//tabs
+const tabsBtn   = document.querySelectorAll(".feautures__tab");
+const tabsItems = document.querySelectorAll(".feautures__content");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+    item.addEventListener("click", function() {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+
+        if( ! currentBtn.classList.contains('active') ) {
+            tabsBtn.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            tabsItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            currentBtn.classList.add('active');
+            currentTab.classList.add('active');
         }
-    }
-    
-    function toggleMenu(element){
-        element.addEventListener('click', () => {
-            checkDisplay();
-        });
+    });
+}
 
-    }
-    toggleMenu(hamburger);
+document.querySelector('.feautures__tab').click();
 
-    //tabs
-    const tabsBtn   = document.querySelectorAll(".feautures__tab");
-    const tabsItems = document.querySelectorAll(".feautures__content");
-    
-    tabsBtn.forEach(onTabClick);
-    
-    function onTabClick(item) {
-        item.addEventListener("click", function() {
-            let currentBtn = item;
-            let tabId = currentBtn.getAttribute("data-tab");
-            let currentTab = document.querySelector(tabId);
-    
-            if( ! currentBtn.classList.contains('active') ) {
-                tabsBtn.forEach(function(item) {
-                    item.classList.remove('active');
-                });
-        
-                tabsItems.forEach(function(item) {
-                    item.classList.remove('active');
-                });
-        
-                currentBtn.classList.add('active');
-                currentTab.classList.add('active');
-            }
-        });
-    }
-    
-    document.querySelector('.feautures__tab').click();
 
-});
